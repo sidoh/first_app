f8ce9151151b6b7c6d42ce9e44145feb81533b95
Sidoh <chris.mullins10@gmail.com>
2011-01-29 03:25:42 -1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smoketest::Application.config.secret_token = '223d1b4e95dafcb15f3e46b7839b7eb1723a0555ab1b07477c745e6cef5916e54b5f24871e115725f10d3a862c8b8938893f90d185a7ce8ff92c631a8f948dde'
